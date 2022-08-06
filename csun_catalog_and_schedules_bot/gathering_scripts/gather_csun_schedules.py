c2a127d475745f39a305259d84ae84fea7674f27
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-08-06 05:18:14 -0700

    noted flag order for webscraper

--- csun_catalog_and_schedules_bot/gathering_scripts/gather_csun_schedules.py
+++ csun_catalog_and_schedules_bot/gathering_scripts/gather_csun_schedules.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 import json
-# Flag Order = Semester(force lower-case) Year SubjectCode(Ignore-Case) Optional: Professor Days Times 
+# Flag Order = Semester Year Position_of_Subject_in_Dropdown Subject_Code
 
 
 class_codes = ["AE","AM","AAS","ACCT","AFRS","AIS","ANTH","ARAB","ARMN","ART","ASTR","AT","ATHL","BANA","BIOL","BLAW","BUS","CE","CADV","CAS","CCE","CD","CECS","CHS","CHEM","CHIN","CIT","CJS","CLAS","CM","COMP","COMS","CTVA","DEAF","EED","ECE","ECON","EDUC","ELPS","ENGL","ENT","EOH","EPC","FCFC","FCHC","FCS","FIN","FLIT","FREN","GBUS","GEOG","GEOL","GWS","HEBR","HHD","HIST","HSCI","HUM","INDS","IS","ITAL","JS","JAPN","JOUR","KIN","KNFC","KOR","LIB","LING","LRS","ME","MATH","MCOM","MGT","MKT","MSE","MUS","NURS","PERS","PHIL","PHSC","PHYS","POLS","PSY","PT","QS","RS","RE","RTM","RUSS","SED","SCI","SCM","SOC","SOM","SPAN","SPED","SUS","SUST","SWRK","TH","UNIV","URBS"]
