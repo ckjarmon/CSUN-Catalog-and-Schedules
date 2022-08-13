d2dd5066fd6ec11d3c8419f7a0e4c5a18da1b964
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-08-13 12:11:14 -0700

    refactored json gather script

--- csun_catalog_and_schedules_bot/server.py
+++ csun_catalog_and_schedules_bot/server.py
@@ -5,46 +5,26 @@ from flask_cors import CORS
 import requests
 import time
 
-# hostName = "localhost"
-# serverPort = 8080
+hostName = "localhost"
+serverPort = 8080
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/json")
+        self.end_headers()
+        self.wfile.write(bytes(open("storedschedules/COMP_schedule.json").read(), "utf-8"))
+        
+        
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+    
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    
+    
+    webServer.server_close()
+    print("Server stopped.")
 
-# class MyServer(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header("Content-type", "text/json")
-#         self.end_headers()
-#         self.wfile.write(bytes(open("storedschedules/COMP_schedule.json").read(), "utf-8"))
-
-
-# if __name__ == "__main__":        
-#     webServer = HTTPServer((hostName, serverPort), MyServer)
-#     print("Server started http://%s:%s" % (hostName, serverPort))
-
-#     try:
-#         webServer.serve_forever()
-#     except KeyboardInterrupt:
-#         pass
-
-#     webServer.server_close()
-#     print("Server stopped.")
-
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        title = str(request.json["title"])
-        body = str(request.json["body"])
-        return jsonify("Sended")
-
-
-CORS(app)
-
-if __name__ == '__main__':
-    app.run()
