482c0c12a25a2b4a164a96fa89891ea25a08ac47
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-08-22 14:55:31 -0700

    implemented spring 2023 for multi subject call

--- csun_catalog_and_schedules_bot/bot.js
+++ csun_catalog_and_schedules_bot/bot.js
@@ -114,7 +114,7 @@ function show_class(subject, code, itchid) {
 }
 
 function show_class(subject, code, semester, year, itchid) {
- 
+
   if (semester !== "spring" && year !== 23) {
     var ret1 = "";
     require("request")({
@@ -210,7 +210,8 @@ function show_class(subject, code, semester, year, itchid) {
       await client.channels.cache.get(itchid).send("```" + ret1 + "```");
     }); //end request
   } else {
-    var ret1 = "", ret2 = "";
+    var ret1 = "",
+      ret2 = "";
 
     require("request")({
       url: 'https://api.metalab.csun.edu/curriculum/api/2.0/terms/Fall-2022/courses/' + subject,
@@ -254,66 +255,66 @@ function show_class(subject, code, semester, year, itchid) {
       }
     }); //end request
 
-   require("request")({
-    url: 'http://127.0.0.1:5000/' + subject,
-    json: true
-  }, async function (error, response, body) {
-    //console.log('https://api.metalab.csun.edu/curriculum/api/2.0/terms/' + semester + '-20' + year + '/classes/' + subject);
-    if (!error && response.statusCode === 200) {
-      //console.log(body); //Print the json response
-      const stuffs = JSON.parse(JSON.stringify(body));
-      //console.log(stuffs.courses[0].title)
-      //ret += stuffs.courses[0];
-      ret2 = ret2.concat("\n\tSection\t\tLocation\t\tDays\t\t  Seats\t\t\t  Time\t\t\t\t\tFaculty\n\t-------\t\t--------\t\t----\t\t  -----\t\t\t  ----\t\t\t\t\t-------\n");
-      stuffs.classes.forEach(element => {
-        if (element.catalog_number === code && element.meetings.length > 0) {
-          ret2 = ret2.concat("\t " + element.class_number);
-          //console.log(element.meetings[0].location);
-          if (element.meetings[0].location.length === 5) {
-            ret2 = ret2.concat("\t\t   " + element.meetings[0].location);
-          } else {
-            ret2 = ret2.concat("\t\t  " + element.meetings[0].location);
+    require("request")({
+      url: 'http://127.0.0.1:5000/' + subject,
+      json: true
+    }, async function (error, response, body) {
+      //console.log('https://api.metalab.csun.edu/curriculum/api/2.0/terms/' + semester + '-20' + year + '/classes/' + subject);
+      if (!error && response.statusCode === 200) {
+        //console.log(body); //Print the json response
+        const stuffs = JSON.parse(JSON.stringify(body));
+        //console.log(stuffs.courses[0].title)
+        //ret += stuffs.courses[0];
+        ret2 = ret2.concat("\n\tSection\t\tLocation\t\tDays\t\t  Seats\t\t\t  Time\t\t\t\t\tFaculty\n\t-------\t\t--------\t\t----\t\t  -----\t\t\t  ----\t\t\t\t\t-------\n");
+        stuffs.classes.forEach(element => {
+          if (element.catalog_number === code && element.meetings.length > 0) {
+            ret2 = ret2.concat("\t " + element.class_number);
+            //console.log(element.meetings[0].location);
+            if (element.meetings[0].location.length === 5) {
+              ret2 = ret2.concat("\t\t   " + element.meetings[0].location);
+            } else {
+              ret2 = ret2.concat("\t\t  " + element.meetings[0].location);
+            }
+
+            if (element.meetings[0].days.length === 1) {
+              ret2 = ret2.concat("\t\t   " + element.meetings[0].days);
+            } else if (element.meetings[0].days.length === 2 || element.meetings[0].days.length === 3) {
+              ret2 = ret2.concat("\t\t  " + element.meetings[0].days);
+            } else {
+              ret2 = ret2.concat("\t\t " + element.meetings[0].days);
+            }
+
+
+
+            ret2 = ret2.concat("\t\t\t " + (element.enrollment_cap - element.enrollment_cap) + "\t\t\t");
+            ret2 = ret2.concat(element.meetings[0].start_time.substring(0, 2) + ":" + element.meetings[0].start_time.substring(2, 4));
+            ret2 = ret2.concat(" - ");
+            ret2 = ret2.concat(element.meetings[0].end_time.substring(0, 2) + ":" + element.meetings[0].end_time.substring(2, 4));
+
+            if (element.instructors.length > 0) {
+              ret2 = ret2.concat("\t\t" + element.instructors[0].instructor);
+            } else {
+              ret2 = ret2.concat("\t\t\t\tStaff");
+            }
+
+            ret2 = ret2.concat("\n");
           }
+        });
 
-          if (element.meetings[0].days.length === 1) {
-            ret2 = ret2.concat("\t\t   " + element.meetings[0].days);
-          } else if (element.meetings[0].days.length === 2 || element.meetings[0].days.length === 3) {
-            ret2 = ret2.concat("\t\t  " + element.meetings[0].days);
-          } else {
-            ret2 = ret2.concat("\t\t " + element.meetings[0].days);
-          }
+      } //end if
 
 
-
-          ret2 = ret2.concat("\t\t\t " + (element.enrollment_cap - element.enrollment_cap) + "\t\t\t");
-          ret2 = ret2.concat(element.meetings[0].start_time.substring(0, 2) + ":" + element.meetings[0].start_time.substring(2, 4));
-          ret2 = ret2.concat(" - ");
-          ret2 = ret2.concat(element.meetings[0].end_time.substring(0, 2) + ":" + element.meetings[0].end_time.substring(2, 4));
-
-          if (element.instructors.length > 0) {
-            ret2 = ret2.concat("\t\t" + element.instructors[0].instructor);
-          } else {
-            ret2 = ret2.concat("\t\t\t\tStaff");
-          }
-
-          ret2 = ret2.concat("\n");
-        }
-      });
-
-    } //end if
-
-      
     }); //end request
 
     //await client.channels.cache.get(itchid).send("```Use !csun " + subject +  " " + code + " spring 23```");
 
-    
+
   }
 }
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
-  
+
   const {
     commandName
   } = interaction;
@@ -417,17 +418,42 @@ client.on('interactionCreate', async interaction => {
     const class2 = (interaction.options.getString('class2')) ? interaction.options.getString('class2').split(" ") : [];
     const class3 = (interaction.options.getString('class3')) ? interaction.options.getString('class3').split(" ") : [];
 
+    sem_bool = interaction.options.getString('semester');
+    year_bool = interaction.options.getString('year');
 
-    show_class(class1[0], class1[1], itchid);
+    var semester = "",
+      year = "";
 
-    if (class2.length !== 0) {
-      show_class(class2[0], class2[1], itchid);
+    if ((sem_bool || year_bool) && !(sem_bool && year_bool)) {
+      await interaction.reply("Need both semester and year if other than Fall 2022.")
+    } else if (sem_bool && year_bool) {
+
+      semester = interaction.options.getString('semester');
+      year = interaction.options.getString('year');
+
+
+      show_class(class1[0], class1[1], semester, year, itchid);
+
+      if (class2.length !== 0) {
+        show_class(class2[0], class2[1], semester, year, itchid);
+      }
+
+      if (class3.length !== 0) {
+        show_class(class3[0], class3[1], semester, year, itchid);
+      }
+    } else {
+
+      show_class(class1[0], class1[1], itchid);
+
+      if (class2.length !== 0) {
+        show_class(class2[0], class2[1], itchid);
+      }
+
+      if (class3.length !== 0) {
+        show_class(class3[0], class3[1], itchid);
+      }
+      await interaction.reply("Gimme a sec");
     }
-
-    if (class3.length !== 0) {
-      show_class(class3[0], class3[1], itchid);
-    }
-    await interaction.reply("Gimme a sec");
   } else if (commandName === 'help') {
 
     let ret = "```\"/class\" for 1 or more classes \n\nSource Code: \
