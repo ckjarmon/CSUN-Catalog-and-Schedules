3fb4b18ad645079983cba6ea30652deadd462615
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-08-29 23:11:51 -0700

    error checking

--- csun_catalog_and_schedules_bot/bot.js
+++ csun_catalog_and_schedules_bot/bot.js
@@ -19,7 +19,9 @@ client.on('ready', () => {
 //   console.log(message.content);
 // });
 function show_prof(subject,prof, itchid) {
-var ret2 = "";
+  const class_codes = ["AE","AM","AAS","ACCT","AFRS","AIS","ANTH","ARAB","ARMN","ART","ASTR","AT","ATHL","BANA","BIOL","BLAW","BUS","CE","CADV","CAS","CCE","CD","CECS","CHS","CHEM","CHIN","CIT","CJS","CLAS","CM","COMP","COMS","CTVA","DEAF","EED","ECE","ECON","EDUC","ELPS","ENGL","ENT","EOH","EPC","FCFC","FCHC","FCS","FIN","FLIT","FREN","GBUS","GEOG","GEOL","GWS","HEBR","HHD","HIST","HSCI","HUM","INDS","IS","ITAL","JS","JAPN","JOUR","KIN","KNFC","KOR","LIB","LING","LRS","ME","MATH","MCOM","MGT","MKT","MSE","MUS","NURS","PERS","PHIL","PHSC","PHYS","POLS","PSY","PT","QS","RS","RE","RTM","RUSS","SED","SCI","SCM","SOC","SOM","SPAN","SPED","SUS","SUST","SWRK","TH","UNIV","URBS"];
+if (class_codes.includes(subject)) 
+{var ret2 = "";
 console.log(subject + " " + prof);
     console.log("Show prof called.");
     var ret1 = "";
@@ -27,10 +29,12 @@ console.log(subject + " " + prof);
       url: `http://127.0.0.1:8000/${subject}/prof`,
       json: true
     }, async function (error, response, body) {
+      console.log(body);
       console.log(`http://127.0.0.1:8000/${subject}/prof`);
       if (!error) {
         const stuffs = JSON.parse(JSON.stringify(body));
         var prof_email = "";
+
         stuffs.profs.forEach(da_prof => {
           if (da_prof.includes(prof.toLowerCase()) && !prof_email) {prof_email = da_prof;}
         }); 
@@ -83,7 +87,7 @@ console.log(subject + " " + prof);
       }
     }); //end request
 
-    setTimeout( async () => {await client.channels.cache.get(itchid).send("```" + ret2 + "```")}, 2000);
+    setTimeout( async () => {await client.channels.cache.get(itchid).send("```" + ret2 + "```")}, 2000);} else {ret_error = `${subject} is not a subject.`;setTimeout( async () => {await client.channels.cache.get(itchid).send("```" + ret_error + "```")}, 2000);}
 }
 
 function show_class(subject, code, itchid) {
