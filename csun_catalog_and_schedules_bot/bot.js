06ec9ba5039372121f718dd72f80986c5ac53151
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-09-01 21:08:54 -0700

    formatting

--- csun_catalog_and_schedules_bot/bot.js
+++ csun_catalog_and_schedules_bot/bot.js
@@ -18,7 +18,7 @@ client.on('ready', () => {
 // client.on('messageCreate', message => {
 //   console.log(message.content);
 // });
-function show_prof(subject,prof, itchid) {
+function show_prof(subject, prof, itchid) {
   const class_codes = ["AE","AM","AAS","ACCT","AFRS","AIS","ANTH","ARAB","ARMN","ART","ASTR","AT","ATHL","BANA","BIOL","BLAW","BUS","CE","CADV","CAS","CCE","CD","CECS","CHS","CHEM","CHIN","CIT","CJS","CLAS","CM","COMP","COMS","CTVA","DEAF","EED","ECE","ECON","EDUC","ELPS","ENGL","ENT","EOH","EPC","FCFC","FCHC","FCS","FIN","FLIT","FREN","GBUS","GEOG","GEOL","GWS","HEBR","HHD","HIST","HSCI","HUM","INDS","IS","ITAL","JS","JAPN","JOUR","KIN","KNFC","KOR","LIB","LING","LRS","ME","MATH","MCOM","MGT","MKT","MSE","MUS","NURS","PERS","PHIL","PHSC","PHYS","POLS","PSY","PT","QS","RS","RE","RTM","RUSS","SED","SCI","SCM","SOC","SOM","SPAN","SPED","SUS","SUST","SWRK","TH","UNIV","URBS"];
 if (class_codes.includes(subject.toUpperCase())) {
 var ret2 = "";
