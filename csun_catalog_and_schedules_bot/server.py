1c81d32beb91dda2dd14e9f677ef455a80978f89
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-10-07 16:30:33 -0700

    gonna use pythons time via flask

--- csun_catalog_and_schedules_bot/server.py
+++ csun_catalog_and_schedules_bot/server.py
@@ -1,10 +1,17 @@
 from flask import Flask
 import json
+import time
 
 app = Flask(__name__)
 
 @app.route('/<string:subject>/<string:data>')
 def get(**kwargs):
     return json.load(open(f'./json_{kwargs["data"]}/{kwargs["subject"].upper()}_{kwargs["data"]}.json'))
-    
-app.run(port=8000)
+
+@app.route('/time')
+def stime():
+    curr_time = time.asctime(time.localtime(time.time())).split()
+    return (f" - As of {curr_time[0]} {curr_time[2]} {curr_time[1]} {curr_time[4]} {curr_time[3]}")
+   
+
+app.run(port=2222)
