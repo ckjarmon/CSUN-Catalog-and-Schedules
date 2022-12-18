23cbbea54cefc05d2d6d413409f24d1f480d8081
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-12-17 16:06:33 -0800

    js bot spacing changes

--- bot.js
+++ bot.js
@@ -277,7 +277,7 @@ function show_class(subject, code, itchid) {
           
 
           ret2 = ret2.concat(`\t\t\t ${(course.enrollment_cap - course.enrollment_count)}\t\t\t${course.start_time.substring(0, 2)}:${course.start_time.substring(2, 4)} - ${course.end_time.substring(0, 2)}:${course.end_time.substring(2, 4)}`);
-          ret2 = (course.instructor !== "Staff") ? ret2.concat("\t\t" + course.instructor) : ret2.concat("\t\t\t\tStaff");
+          ret2 = (course.instructor !== "Staff") ? ret2.concat("\t\t\t" + course.instructor) : ret2.concat("\t\t\t\tStaff");
           ret2 = ret2.concat("\n");
         
       });
