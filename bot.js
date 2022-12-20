d12ad165a7c66a7f27bfb829412112a57192e423
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-12-19 17:50:33 -0800

    nope still messed up

--- bot.js
+++ bot.js
@@ -233,7 +233,7 @@ function show_class(subject, code, itchid) {
         } else if (course.location.length > 5) {
           ret2 += (`\t\t  ${course.location}`)
         } else {
-          ret2 += (`\t\t      ${course.location}`)
+          ret2 += (`\t\t     ${course.location}`)
         }
 
 
