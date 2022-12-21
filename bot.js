00eb0d8f1e25c6401e18a3b64f455576a1ba53bb
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-12-20 20:58:08 -0800

    updated help message

--- bot.js
+++ bot.js
@@ -540,6 +540,7 @@ client.on('interactionCreate', async interaction => {
         "\"/classes\" for 1 or more classes of different subjects \n\n" +
         "\"/prof\" to show a prof's teaching schedule \n\n" +
         "\"/level\" to show classes at a specific level (100, 200, 300 etc.) \n\n" +
+        "\"/gunfight\" to time somebody out for 10 secs \n\n" +
         "Source Code:\nhttps://github.com/kyeou/Python-Scripts/tree/main/csun_catalog_and_schedules_bot```";
 
       await interaction.reply(ret);
