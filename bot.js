42fa440e8cff26fadef62beea398ecb581451d91
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-01-24 19:32:27 -0800

    ...

--- bot.js
+++ bot.js
@@ -597,13 +597,13 @@ client.on('interactionCreate', async interaction => {
       const user = interaction.options.getUser('target');
 
       const member = interaction.guild.members.cache.get(user.id);
-      //if (member.id !== "534510030490304524") { 
+      if (member.id !== "534510030490304524") { 
         member.timeout(10000, "bleh"); 
-        interaction.reply(`\`\`\`${user.username} has been timed out!\`\`\``); //}
-     // else { 
-     //   interaction.reply('```Kyeou is immune.```'); 
-     // }
-      
+        interaction.reply(`\`\`\`${user.username} has been timed out!\`\`\``); }
+      else { 
+       interaction.reply('```Kyeou is immune.```'); 
+     }
+
 
     }
   }
