41469f49641da3538ae21b7d469e758317b36546
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-05-21 20:29:01 -0700

    change service paths to absolute

--- main/run.sh
+++ main/run.sh
@@ -3,13 +3,13 @@
 if [ "$1" == "stop" ]; then
     # stop background processes if stop command is passed
     echo "Stopping background processes"
-    pkill -f "while :; do tsc -p ~/CSUN-Catalog-And-Schedules/ && node ~/CSUN-Catalog-And-Schedules/run/main/src/bot.js; done"
-    pkill -f "tsc -p ~/CSUN-Catalog-And-Schedules && node ~/CSUN-Catalog-And-Schedules/run/main/src/server.js"
+    pkill -f "while :; do tsc -p /home/kyeou//CSUN-Catalog-And-Schedules/ && node /home/kyeou//CSUN-Catalog-And-Schedules/run/main/src/bot.js; done"
+    pkill -f "tsc -p /home/kyeou//CSUN-Catalog-And-Schedules && node /home/kyeou//CSUN-Catalog-And-Schedules/run/main/src/server.js"
     exit 0
 fi
 
 
-while :; do tsc -p ~/CSUN-Catalog-And-Schedules/ && node ~/CSUN-Catalog-And-Schedules/run/main/src/bot.js; done &
-tsc -p ~/CSUN-Catalog-And-Schedules && node ~/CSUN-Catalog-And-Schedules/run/main/src/server.js &
+while :; do tsc -p /home/kyeou//CSUN-Catalog-And-Schedules/ && node /home/kyeou//CSUN-Catalog-And-Schedules/run/main/src/bot.js --config /home/kyeou//CSUN-Catalog-And-Schedules/main/src/config.json; done &
+tsc -p /home/kyeou//CSUN-Catalog-And-Schedules && node /home/kyeou//CSUN-Catalog-And-Schedules/run/main/src/server.js &
 
 
