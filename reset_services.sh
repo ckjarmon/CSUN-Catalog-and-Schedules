3eba85495752a2c2fedf2e88155e464df20e2c88
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-05-21 21:09:35 -0700

    r/ihadastroke

--- reset_services.sh
+++ reset_services.sh
@@ -1,6 +1,6 @@
 git pull
-sudo cp -f csun_cat_and_sch.servive /etc/systemd/system
-sudo cp -f jobs/crawler/crawler.serive /etc/systemd/system
+sudo cp -f csun_cat_and_sch.service /etc/systemd/system
+sudo cp -f jobs/crawler/crawler.service /etc/systemd/system
 
 sudo systemctl daemon-reload
 
