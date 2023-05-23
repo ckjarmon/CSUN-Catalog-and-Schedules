3a8bef8345105cae5c5707a2720e25aafe73f1c8
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-05-23 15:41:57 -0700

    cleared up some types in server and did wildcard queries since they return objects, added a logger

--- main/src/server.ts
+++ main/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { Pool, PoolClient } from "pg";
 // import argparse from "argparse";
-import { Logger } from "tslog";
+import logger from "./logger";
 const app = express();
 
 /*
@@ -19,7 +19,7 @@ if (args.project_location) {
 	process.chdir(args.project_location);
 }*/
 
-const log = new Logger({
+/*const log = new Logger({
 	stylePrettyLogs: true,
 	prettyLogStyles: {
 		logLevelName: {
@@ -33,7 +33,33 @@ const log = new Logger({
 			FATAL: ["bold", "redBright"]
 		}
 	}
-});
+});*/
+
+interface Professor {
+	email: string;
+	first_name: string;
+	last_name: string;
+	image_link: string;
+	phone_number: string;
+	location: string;
+	website: string;
+	mail_drop: string;
+	subject: string;
+	office: string;
+}
+
+interface Schedule {
+	class_number: number;
+	enrollment_cap: number;
+	enrollment_count: number;
+	instructor: string;
+	days: string;
+	location: string;
+	start_time: string;
+	end_time: string;
+	catalog_number: string;
+	subject: string;
+}
 
 const name_normalize = (str: string): string => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
 
@@ -41,9 +67,9 @@ const pool = new Pool({
 	user: "kyeou",
 	password: "q1w2e3r4!@#$",
 	host: "127.0.0.1",
-	port: 5432, 
+	port: 5432,
 	database: "csun",
-	max: 5 
+	max: 5
 });
 
 const get_conn = async (): Promise<PoolClient> => {
@@ -57,40 +83,33 @@ const get_conn = async (): Promise<PoolClient> => {
 };
 
 app.get("/:subject-:catalog_number/catalog", async (req: Request, res: Response) => {
-	log.info(`~Endpoint called: ${req.originalUrl}`);
+	// log.info(`~Endpoint called: ${req.originalUrl}`);
 	const { subject, catalog_number } = req.params;
 	const rootCursor = await get_conn();
 	try {
-		const query = `SELECT 
-                        subject, 
-                        catalog_number, 
-                        title, 
-                        description, 
-                        units, 
-                        prerequisites, 
-                        corequisites
-                        FROM catalog 
-                        WHERE subject = '${subject.toUpperCase()}' 
-                        AND catalog_number = '${catalog_number}'`;
-		const x = await rootCursor.query(query);
+		const query = `SELECT * FROM catalog WHERE subject = '${subject.toUpperCase()}' AND catalog_number = '${catalog_number}'`;
+
+		const le_fetch = (await rootCursor.query(query)).rows[0];
 		// console.log(x)
-		res.json(x.rows[0]);
+		logger.http(`Endpoint: ${req.originalUrl}\n\tHTTP: 200`);
+		res.json(le_fetch);
 	} catch (err) {
-		console.error(err);
-		res.sendStatus(500);
+		// console.error(err);
+		logger.error(`Endpoint: ${req.originalUrl}\n\tHTTP: 500`);
+		res.status(500).send("Internal Server Error");
 	} finally {
 		rootCursor.release();
 	}
 });
 
 app.get("/:subject/levels/:level", async (req: Request, res: Response) => {
-	log.info(`~Endpoint called: ${req.originalUrl}`);
+	// log.info(`~Endpoint called: ${req.originalUrl}`);
+	const rootCursor = await get_conn();
 	try {
-		const rootCursor = await get_conn();
-		const subject = req.params.subject.toUpperCase();
-		const level = req.params.level[0];
+		const subject: string = req.params.subject.toUpperCase();
+		const level: string = req.params.level[0];
 
-		const query = `SELECT
+		const query: string = `SELECT
                         subject,
                         catalog_number,
                         title
@@ -98,126 +117,86 @@ app.get("/:subject/levels/:level", async (req: Request, res: Response) => {
                         WHERE subject = '${subject}'
                         AND catalog_number LIKE '${level}%';`;
 
-		const fetch = await rootCursor.query(query);
+		const le_fetch = (await rootCursor.query(query)).rows[0];
 		// console.log(fetch)
-		const results = fetch.rows[0].map(
+		const results = le_fetch.map(
 			(x: { subject: string; catalog_number: string; title: string }) =>
 				`${x.subject} ${x.catalog_number} - ${x.title}`
 		);
-
+		logger.http(`Endpoint: ${req.originalUrl}\n\tHTTP: 200`);
 		res.send(results);
 	} catch (err) {
-		console.error(err);
+		// console.error(err);
+		logger.error(`Endpoint: ${req.originalUrl}\n\tHTTP: 500`);
 		res.status(500).send("Internal Server Error");
+	} finally {
+		rootCursor.release();
 	}
 });
 
 app.get("/:subject-:catalog_number/:semester-:year/schedule", async (req: Request, res: Response) => {
-	log.info(`~Endpoint called: ${req.originalUrl}`);
+	// log.info(`~Endpoint called: ${req.originalUrl}`);
 	const { subject, catalog_number, semester, year } = req.params;
 	const rootCursor = await get_conn();
 	try {
-		const query = `SELECT 
-                        class_number, 
-                        enrollment_cap, 
-                        enrollment_count,
-						waitlist_cap, 
-                        waitlist_count,  
-                        instructor, 
-                        days, 
-                        location,
-                        start_time, 
-                        end_time, 
-                        catalog_number, 
-                        subject 
-                        FROM section 
-                        WHERE subject = '${subject.toUpperCase()}' 
-                        AND catalog_number = '${catalog_number}' 
-                        AND semester = '${semester}' 
-                        AND year = ${year}`;
-		const le_fetch = await rootCursor.query(query);
-		res.json(le_fetch.rows);
+		const query: string = `SELECT * FROM section 
+		WHERE subject = '${subject.toUpperCase()}' 
+		AND catalog_number = '${catalog_number}' 
+		AND semester = '${semester}'  
+		AND year = ${year}`;
+
+		const le_fetch = (await rootCursor.query(query)).rows;
+		logger.http(`Endpoint: ${req.originalUrl}\n\tHTTP: 200`);
+		res.json(le_fetch);
 	} catch (err) {
-		console.error(err);
-		res.sendStatus(500);
+		// console.error(err);
+		logger.error(`Endpoint: ${req.originalUrl}\n\tHTTP: 500`);
+		res.status(500).send("Internal Server Error");
 	} finally {
 		rootCursor.release();
 	}
 });
 
 app.get("/profs/:subject/:id?", async (req: Request, res: Response) => {
-	log.info(`~Endpoint called: ${req.originalUrl}`);
+	// log.info(`~Endpoint called: ${req.originalUrl}`);
 	const rootCursor = await get_conn();
 	const subject = req.params.subject.toUpperCase();
-	if (req.params.id) {
-		const query = `SELECT first_name, last_name FROM professor WHERE subject = '${subject}'`;
-		const rows: { first_name: string; last_name: string }[] = (await rootCursor.query(query)).rows;
-		const profs_as_first_last: string[] = rows.map(
-			(x: { first_name: string; last_name: string }) => `${x.first_name} ${x.last_name}`
-		);
-		const sorted_profs_as_first_last = profs_as_first_last.sort((a: string, b: string) =>
-			name_normalize(a.split(" ")[1]) < name_normalize(b.split(" ")[1]) ? -1 : 1
-		);
-		const id = Number(req.params.id) - 1;
-		const prof_query = `SELECT 
-                            email, 
-                            first_name, 
-                            last_name, 
-                            image_link, 
-                            phone_number, 
-                            location, 
-                            website, 
-                            mail_drop, 
-                            subject, 
-                            office 
-                            FROM professor 
-                            WHERE first_name = '${sorted_profs_as_first_last[id].split(" ")[0]}' 
-                            AND last_name = '${sorted_profs_as_first_last[id].split(" ")[1]}'`;
-		const query_result = await rootCursor.query(prof_query);
-		const prof_rows = query_result.rows;
-		const p = {
-			email: prof_rows[0].email,
-			first_name: name_normalize(prof_rows[0].first_name),
-			last_name: name_normalize(prof_rows[0].last_name),
-			image_link: prof_rows[0].image_link || "N/A",
-			phone_number: prof_rows[0].phone_number || "N/A",
-			location: prof_rows[0].location || "N/A",
-			website: prof_rows[0].website || "N/A",
-			mail_drop: prof_rows[0].mail_drop || "N/A",
-			subject: prof_rows[0].subject || "N/A",
-			office: prof_rows[0].office || "N/A"
-		};
-		const section_query = `SELECT 
-                                class_number, 
-                                enrollment_cap, 
-                                enrollment_count, 
-                                instructor, 
-                                days, 
-                                location, 
-                                start_time, 
-                                end_time, 
-                                catalog_number, 
-                                subject 
-                                FROM section 
-								WHERE instructor ILIKE '%${p.first_name.split(",")[0]}%' 
-								AND instructor ILIKE '%${p.last_name.split(",")[0]}%'
-                                AND subject = '${subject.toUpperCase()}' 
-                                AND semester = 'fall' AND year = '2023'`;
-		const query_result_2 = await rootCursor.query(section_query);
-		const section_rows = query_result_2.rows;
-		const sch = section_rows.map(
-			(c: {
-				class_number: number;
-				enrollment_cap: number;
-				enrollment_count: number;
-				instructor: string;
-				days: string;
-				location: string;
-				start_time: string;
-				end_time: string;
-				catalog_number: string;
-				subject: string;
-			}) => ({
+	try {
+		if (req.params.id) {
+			const query: string = `SELECT first_name, last_name FROM professor WHERE subject = '${subject}'`;
+			const rows: { first_name: string; last_name: string }[] = (await rootCursor.query(query))
+				.rows;
+			const profs_as_first_last: string[] = rows.map(
+				(x: { first_name: string; last_name: string }) => `${x.first_name} ${x.last_name}`
+			);
+			const sorted_profs_as_first_last: string[] = profs_as_first_last.sort(
+				(a: string, b: string) =>
+					name_normalize(a.split(" ")[1]) < name_normalize(b.split(" ")[1]) ? -1 : 1
+			);
+			const id: number = Number(req.params.id) - 1;
+			const prof_query: string = `SELECT * FROM professor WHERE first_name = '${
+				sorted_profs_as_first_last[id].split(" ")[0]
+			}' AND last_name = '${sorted_profs_as_first_last[id].split(" ")[1]}'`;
+			const prof_rows: Professor = (await rootCursor.query(prof_query)).rows[0];
+			const p = {
+				email: prof_rows.email,
+				first_name: name_normalize(prof_rows.first_name),
+				last_name: name_normalize(prof_rows.last_name),
+				image_link: prof_rows.image_link || "N/A",
+				phone_number: prof_rows.phone_number || "N/A",
+				location: prof_rows.location || "N/A",
+				website: prof_rows.website || "N/A",
+				mail_drop: prof_rows.mail_drop || "N/A",
+				subject: prof_rows.subject || "N/A",
+				office: prof_rows.office || "N/A"
+			};
+			const section_query: string = `SELECT * FROM section  WHERE instructor ILIKE '%${
+				p.first_name.split(",")[0]
+			}%' AND instructor ILIKE '%${
+				p.last_name.split(",")[0]
+			}%' AND subject = '${subject.toUpperCase()}'  AND semester = 'fall' AND year = '2023'`;
+			const section_rows: Schedule[] = (await rootCursor.query(section_query)).rows;
+			const schedule = section_rows.map((c: Schedule) => ({
 				class_number: c.class_number,
 				enrollment_cap: c.enrollment_cap,
 				enrollment_count: c.enrollment_count,
@@ -228,31 +207,38 @@ app.get("/profs/:subject/:id?", async (req: Request, res: Response) => {
 				end_time: c.end_time,
 				catalog_number: c.catalog_number,
 				subject: c.subject
-			})
-		);
-		const result = {
-			info: p,
-			sch
-		};
-		res.send(result);
-	} else {
-		const subject = req.params.subject.toUpperCase();
-		const query = `SELECT first_name, last_name FROM professor WHERE subject = '${subject}'`;
-		const rows = await rootCursor.query(query);
+			}));
+			const result = {
+				info: p,
+				schedule
+			};
+			res.json(result);
+		} else {
+			const subject: string = req.params.subject.toUpperCase();
+			const query: string = `SELECT first_name, last_name FROM professor WHERE subject = '${subject}'`;
+			const rows = (await rootCursor.query(query)).rows;
 
-		const profs = rows.rows
-			.map(
-				(row: { first_name: string; last_name: string }) =>
-					`${name_normalize(row.first_name)} ${name_normalize(row.last_name)}`
-			)
-			.sort((a: string, b: string) => a.split(" ")[1].localeCompare(b.split(" ")[1]));
+			const profs = rows
+				.map(
+					(row: { first_name: string; last_name: string }) =>
+						`${name_normalize(row.first_name)} ${name_normalize(row.last_name)}`
+				)
+				.sort((a: string, b: string) => a.split(" ")[1].localeCompare(b.split(" ")[1]));
 
-		const response = profs.map((prof: any, index: number) => `${index + 1} ${prof}\n`).join("");
-		res.send(response);
+			const response = profs.map((prof: any, index: number) => `${index + 1} ${prof}\n`).join("");
+			res.send(response);
+		}
+		logger.http(`Endpoint: ${req.originalUrl}\n\tHTTP: 200`);
+	} catch {
+		// console.error(err);
+		logger.error(`Endpoint: ${req.originalUrl}\n\tHTTP: 500`);
+		res.status(500).send("Internal Server Error");
+	} finally {
+		rootCursor.release();
 	}
 });
 
 app.listen(2222, () => {
-	log.silly(`Running on http://localhost:2222`);
+	logger.info(`Running on http://localhost:2222`);
 	// console.log("Running...");
 });
