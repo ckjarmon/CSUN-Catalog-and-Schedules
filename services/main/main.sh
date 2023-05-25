320d7902696cb94788660acda8765feb373b03ed
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-05-24 20:56:12 -0700

    chnagged run location of server in sh script

--- services/main/main.sh
+++ services/main/main.sh
@@ -3,13 +3,13 @@
 if [ "$1" == "stop" ]; then
     # stop background processes if stop command is passed
     echo "Stopping background processes"
-    pkill -f "while :; do tsc -p /home/kyeou/CSUN-Catalog-And-Schedules/ && node /home/kyeou/CSUN-Catalog-And-Schedules/run/main/src/bot.js; done"
-    pkill -f "tsc -p /home/kyeou/CSUN-Catalog-And-Schedules && node /home/kyeou/CSUN-Catalog-And-Schedules/run/main/src/server.js"
+    pkill -f "while :; do node /home/kyeou/CSUN-Catalog-And-Schedules/main/obj/src/bot.js --config /home/kyeou/CSUN-Catalog-And-Schedules/main/src/config.json; done"
+    pkill -f "node /home/kyeou/CSUN-Catalog-And-Schedules/main/obj/src/server.js &"
     exit 0
 fi
 
 
 while :; do node /home/kyeou/CSUN-Catalog-And-Schedules/main/obj/src/bot.js --config /home/kyeou/CSUN-Catalog-And-Schedules/main/src/config.json; done &
-node /home/kyeou/CSUN-Catalog-And-Schedules/run/main/src/server.js &
+node /home/kyeou/CSUN-Catalog-And-Schedules/main/obj/src/server.js &
 
 
