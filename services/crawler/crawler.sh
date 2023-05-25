4320c16455e5a872fc1fb8ce24b1c716f52d1a4e
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-05-24 20:54:15 -0700

    dont do pnpm installs automatically

--- services/crawler/crawler.sh
+++ services/crawler/crawler.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 function start_crawler() {
-  cd /home/kyeou/CSUN-Catalog-And-Schedules/jobs/crawler
-  mkdir obj
-  sudo pnpm install && tsc 
   node /home/kyeou/CSUN-Catalog-And-Schedules/jobs/crawler/obj/src/run.js --semester_key 2237
 }
 
