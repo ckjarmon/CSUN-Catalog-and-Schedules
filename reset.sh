39a08a43f56bb1e4c4e4ca5690f1bb22274112c2
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-05-24 18:45:35 -0700

    run some of these in the background

--- reset.sh
+++ reset.sh
@@ -1,11 +1,11 @@
 git pull
 sudo rm -rf node_modules
 pnpm install
-sudo cp -f services/main/csun_catalog_and_schedules.service /etc/systemd/system
-sudo cp -f services/crawler/crawler.service /etc/systemd/system
+sudo cp -f services/main/csun_catalog_and_schedules.service /etc/systemd/system &
+sudo cp -f services/crawler/crawler.service /etc/systemd/system &
 
 sudo systemctl daemon-reload
 
-sudo systemctl restart csun_catalog_and_schedules
-sudo systemctl restart crawler
+sudo systemctl restart csun_catalog_and_schedules &
+sudo systemctl restart crawler &
 
