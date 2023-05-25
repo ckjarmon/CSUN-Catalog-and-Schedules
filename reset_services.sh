f3bae29f2cae640c55a4614e3a1b4e2eb8837637
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-05-24 18:41:29 -0700

    reworked services and their bash scripts

--- reset_services.sh
+++
@@ -1,9 +0,0 @@
-git pull
-sudo cp -f csun_cat_and_sch.service /etc/systemd/system
-sudo cp -f jobs/crawler/crawler.service /etc/systemd/system
-
-sudo systemctl daemon-reload
-
-sudo systemctl restart csun_cat_and_sch
-sudo systemctl restart crawler
-
