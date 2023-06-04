03e52962c277b559f5c809c785a25fd131aacd1d
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-06-04 10:03:31 -0700

    ...

--- services/bot/bot.sh
+++ services/bot/bot.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 if [ "$1" == "stop" ]; then
-    # stop background processes if stop command is passed
-    echo "Stopping background processes"
     pkill -f "while :; do node /home/kyeou/CSUN-Catalog-And-Schedules/bot/obj/src/bot.js --config /home/kyeou/CSUN-Catalog-And-Schedules/bot/config.json; done"
     pkill -f "node /home/kyeou/CSUN-Catalog-And-Schedules/bot/obj/src/server.js"
     exit 0
