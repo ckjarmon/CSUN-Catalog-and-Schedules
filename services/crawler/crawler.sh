fab20109adbab431c864f5ea81ef4c1f34cf7034
Christian Jarmon <christianjarmon630@gmail.com>
2023-06-18 15:13:37 -0700

    changes argument names for crawler

--- services/crawler/crawler.sh
+++ services/crawler/crawler.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function start_crawler() {
-  node /home/kyeou/CSUN-Catalog-And-Schedules/jobs/crawler/obj/src/run.js --semester_key 2237 &
-  node /home/kyeou/CSUN-Catalog-And-Schedules/jobs/crawler/obj/src/run.js --semester_key 2235 &
+  node /home/kyeou/CSUN-Catalog-And-Schedules/jobs/crawler/obj/src/run.js -k 2237 &
+  node /home/kyeou/CSUN-Catalog-And-Schedules/jobs/crawler/obj/src/run.js -k 2235 &
 }
 
 tsc -p /home/kyeou/CSUN-Catalog-And-Schedules/jobs/crawler
